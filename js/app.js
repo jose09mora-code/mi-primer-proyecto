7b14046febaf89f2b835aadb617232baa4761cc2
Jose Mora <jose09mora@gmail.com>
2026-08-09 12:40:03 +0200

    Añadir modo oscuro

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 const mensaje = document.querySelector("#mensaje");
 const botonMensaje = document.querySelector("#boton-mensaje");
+const botonTema = document.querySelector("#boton-tema");
 
 botonMensaje.addEventListener("click", () => {
     mensaje.textContent = "¡He conseguido modificar mi primera aplicación web!";
     botonMensaje.textContent = "¡Hecho!";
+});
+
+botonTema.addEventListener("click", () => {
+    document.body.classList.toggle("tema-oscuro");
+    const modoOscuroActivo = document.body.classList.contains("tema-oscuro");
+    botonTema.textContent = modoOscuroActivo ? "Modo claro" : "Modo oscuro";
 });
